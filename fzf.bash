7da09a566b8ade7586352a20f6264305fd6a3cc4
max furman <mx.furman@gmail.com>
2017-06-20 22:44:53 -0700

    key bindings moved to fzf profile

--- fzf.bash
+++ fzf.bash
@@ -9,7 +9,3 @@ fi
 # Auto-completion
 # ---------------
 [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/usr/local/opt/fzf/shell/key-bindings.bash"
