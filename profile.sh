9dce64cd8b1bf399b6aed94359f3580ec214eb8e
max furman <mx.furman@gmail.com>
2017-06-20 17:43:11 -0700

    osx specific bash prompt

--- profile.sh
+++ profile.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+. $HOME/.profile.d/colors.sh
+. $HOME/.profile.d/git-completion.sh
+. $HOME/.profile.d/git-prompt.sh
+
 # --- Aliases
 alias grep='grep --color=auto -r'
 alias hangoutsrage='sudo killall AppleCameraAssistant'
@@ -18,3 +22,17 @@ GOSRC=/usr/local/Cellar/go/1.1
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
     . $(brew --prefix)/etc/bash_completion
 fi
+
+# --- Prompt
+POINTER_CHAR="⬆"
+PREINFO="\n$POINTER_CHAR $BIBLUE[ "$BLUE"exit: $WHITE\$?$BIBLUE ] [ "$BLUE"time: $WHITE\t$BIBLUE ]$END_COLOR"
+PREGIT="$PREINFO\n$WHITE\u $PURPLE@ $BLUE\h$WHITE \w$END_COLOR"
+POSTGIT=" $RED\$$END_COLOR "
+
+export PROMPT_COMMAND="__git_ps1 \"$PREGIT\" \"$POSTGIT\""
+export GIT_PS1_SHOWDIRTYSTATE=true
+export GIT_PS1_SHOWSTASHSTATE=true
+export GIT_PS1_SHOWCOLORHINTS=true
+export GIT_PS1_SHOWUNTRACKEDFILES=true
+export GIT_PS1_SHOWUPSTREAM="auto"
+# ---
